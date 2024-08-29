344b56427a296e825eb59fa2e107069059dbf3b4
BryanLS736 <bryanlserna07@gmail.com>
2024-08-28 21:32:43 -0500

    new

---
+++ Reforzamiento 2/12.py
@@ -0,0 +1,12 @@
+"""Creamos nuestras dos listas"""
+
+lista_uno = [5, 4.4, 'Bryan', True]
+lista_dos = [2, 4, 6, 8, 10]
+
+"""Sumamos las 2 listas"""
+
+lista_suma = lista_uno + lista_dos
+
+"""Imprimimos la suma de las dos listas"""
+
+print('La suma de las 2 listas es: {}'.format(lista_suma))
